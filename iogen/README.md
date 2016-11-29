c43d3f3c8200fa69fa25548ab141063867a8e6b3
bbyalcinkaya <bbyalcinkaya@gmail.com>
2016-11-29 05:10:35 +0300

    Add iogen

---
+++ iogen/README.md
@@ -0,0 +1,6 @@
+# iogen
+Runs input file generator program with given arguments and solver with generated input.
+This script is used to generate input/output sets for programming contests.
+
+## How to use it
+Just set the variables in iogen.py and run. 
---
+++ iogen/iogen.py
@@ -0,0 +1,45 @@
+import os
+
+# Args
+generator_args = [
+  (3,"foo"),
+  (2,"bar"),
+  (1,"baz")
+]
+"""
+generator_args = [
+  (n, arg),
+  ...
+]
+Runs generator with argument arg n times and so on
+"""
+
+# Paths
+generator = "echo"         
+solver = "echo yah"        
+input_dir = "Example/input"         
+output_dir = "Example/output"     
+
+# Create Inputs
+if not os.path.isdir(input_dir):
+    os.mkdir(input_dir)
+
+
+nfiles = 1
+for (times, arg) in generator_args:
+    for i in range(times):
+        os.system("{} {} > {}/{}.txt".format(generator, arg, input_dir, nfiles))
+        print("generated {} {}".format(nfiles, arg))
+        nfiles += 1
+nfiles -= 1
+
+
+
+# Create outputs
+if not os.path.isdir(output_dir):
+    os.mkdir(output_dir)
+for i in range(1,nfiles+1):
+    os.system("{} < {}/{}.txt > {}/{}.txt".format(solver, input_dir, i, output_dir, i))
+    print("solved {}".format(i))
+
+ 
